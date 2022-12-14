c7f7f1b79d884d244c868425ccefd36603ecfa9f
narcissus313 <srz.shl@gmail.com>
2022-12-14 20:39:56 +0330

    all files added

--- 1_1/rally.js
+++ 1_1/rally.js
@@ -1,17 +1,25 @@
 function rally() {
 	let carNumbers = +prompt("Enter the number of cars to start the rally:");
-	let playZone = new Array(300).fill("*");
+	// let playZone = new Array(300).fill("*");
 	let cars = [];
 	for (let i = 0; i < carNumbers; i++)
 		cars[i] = prompt("Enter the car name:");
-	return cars;
+
+	let sequence = [];
+	while (sequence.length < cars.length) {
+		let index = Math.floor(Math.random() * cars.length);
+		if (sequence.includes(cars[index])) continue;
+		sequence.push(cars[index]);
+	}
+	return sequence;
 }
 
 console.log(rally());
-
-// function user(name, age) {
-//     this.name = name;
-//     this.age = age;
+// let cars = ['a', 'b', 'c', 'd'];
+// let sequence = [];
+// while (sequence.length < cars.length) {
+// 	let index = Math.floor(Math.random() * cars.length);
+// 	if (sequence.includes(cars[index])) continue;
+// 	sequence.push(cars[index]);
 // }
-// let reza = new user('reza', 34);
-// console.log(reza);
+// console.log(sequence);
